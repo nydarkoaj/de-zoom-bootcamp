d00423a764e40736aa0c09d5687a2536e1a316d8
Nana Yaw <nana.darko@amalitech.com>
2026-03-01 18:38:04 +0000

    add basic pipeline scripts

--- pipeline/pipeline.py
+++ pipeline/pipeline.py
@@ -9,7 +9,7 @@ print(f'pipeline month = {month}')
 
 df = pd.DataFrame({"day": [1,2], "num_passengers":[3,5]})
 df['month'] = month
-
+                    
 print(df)
 
 df.to_parquet(f"output_{month}.parquet")
